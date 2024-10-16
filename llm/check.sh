885d37e305e6a761a03d3c2136a0aa91b6288026
Zhong Hui <zhonghui.net@gmail.com>
2024-10-16 15:58:57 +0800

    Update check.sh

--- llm/check.sh
+++ llm/check.sh
@@ -1,4 +1,4 @@
-grep "step: 500," log_szzj-inf-sci-k8s-a800-hbxgn6-0541.szzj.baidu.com/workerlog.0 |tail -1 | awk -F ',' '{print $9}' | awk -F ":" '{print $2}'  | while read -r num_str; do  
+grep "step: 500," ./log/workerlog.0 |tail -1 | awk -F ',' '{print $9}' | awk -F ":" '{print $2}'  | while read -r num_str; do  
     if [ "$num_str" -lt 7200 ]; then  
         echo "Assertion passed: $num_str is less than 7200"  
         exit 1
